1155f3701676eacd4eb6cbb0daeaad6fcb55165d
Jarrell Porter <jarrell_porter@optum.com>
2022-07-05 00:06:14 -0500

    added extension parameter to get_chaptered(). Cleaned up global variables. Added copy to join_chaptered() if only one video was available for an ID.

--- gopro.py
+++ gopro.py
@@ -4,26 +4,23 @@ import subprocess
 import logging
 import datetime
 import sys
+import shutil
 
 from pathlib import Path
-is_main = __name__ == '__main__'
-log_file = f'gopro_logs_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.txt'
-if is_main:
-    if sys.platform == 'win32':
-        output = os.environ['USERPROFILE']
-    if sys.platform == 'linux':
-        output = os.environ['HOME']
-    output = os.path.join(output,'Videos','gopro')
-    if not os.path.exists(output):
-        os.mkdir(output)
-    logs = os.path.join(
-        output
-        ,log_file
-        )
-else:
-    logs = os.path.join(os.path.dirname(__name__),log_file)
-    output = 'somePath'
 
+log_file = f'gopro_logs_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.txt'
+if sys.platform == 'win32':
+    output = os.environ['USERPROFILE']
+if sys.platform == 'linux':
+    output = os.environ['HOME']
+output = os.path.join(output,'Videos','gopro')
+if not os.path.exists(output):
+    os.mkdir(output)
+
+logs = os.path.join(
+    output
+    ,log_file
+    )
 file_handler = logging.FileHandler(filename=logs)
 stdout_handler = logging.StreamHandler(sys.stdout)
 handlers = [file_handler, stdout_handler]
@@ -37,7 +34,7 @@ logging.basicConfig(
     ,level=logging.DEBUG
     )
 
-def get_chaptered(path:str) -> list:
+def get_chaptered(path:str,ext:str='.mp4') -> list:
     """
     Used to group and order gopro .mp4 files. Returns dict of files grouped by fileid.
     
@@ -58,52 +55,56 @@ def get_chaptered(path:str) -> list:
     https://community.gopro.com/s/article/GoPro-Camera-File-Naming-Convention?language=en_US
     """
     files={}
-    logging.info(f"Processing files in directory: {path}")
-    for file in glob.glob(os.path.join(path,"*.mp4")):
+    logging.info(f"Pairing files with extension {ext} in directory: {path}")
+    for file in glob.glob(os.path.join(path,"*"+ext)):
         file = Path(file)
         fileid = file.stem[4:]
         chapter = file.stem[2:4]
         if not fileid in files.keys():
             files[fileid] = {}
-        if chapter == 'PR':
+        if chapter.upper() == 'PR':
             chapter = 0
         else:
             chapter = int(chapter)
         files[fileid][chapter] = str(file)
     return files        
 
-def join_chaptered(d:dict,dest:str) -> None:
+def join_chaptered(d:dict,dest:str=output) -> None:
     """
     joins files grouped by `get_chaptered()`
     """
     for fileid,files in d.items():
         output = os.path.join(dest,fileid+'.mp4')
         fileorder=[]
-        for i in range(len(files)):
-            fileorder.append(
-                "file '"+
-                'file:'+files[i].replace(
-                '\\','/').replace(
-                    "'","'\\''") + "'"
+        if len(files)==1:
+            shutil.copy(files[0],dest)
+            logging.info(f"Only 1 file for id {fileid}. Copying to {dest}")
+        else:
+            for i in range(len(files)):
+                fileorder.append(
+                    "file '"+
+                    'file:'+files[i].replace(
+                    '\\','/').replace(
+                        "'","'\\''") + "'"
+                )
+            file_cmd = os.path.join(dest,fileid+'.txt')
+            with open(file_cmd,'w') as f:
+                f.write('\n'.join(fileorder))
+            logging.info('merging files:\n\t{0}'.format('\n\t'.join(files.values())))
+            process_output = subprocess.Popen(
+                ['ffmpeg'
+                ,'-safe','0'
+                ,'-f','concat'
+                ,'-i',file_cmd
+                ,'-c','copy',output]
+                # ,capture_output=True
+                # ,text=True
+                ,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE
             )
-        file_cmd = os.path.join(dest,fileid+'.txt')
-        with open(file_cmd,'w') as f:
-            f.write('\n'.join(fileorder))
-        logging.info('merging files:\n\t{0}'.format('\n\t'.join(files.values())))
-        process_output = subprocess.Popen(
-            ['ffmpeg'
-            ,'-safe','0'
-            ,'-f','concat'
-            ,'-i',file_cmd
-            ,'-c','copy',output]
-            # ,capture_output=True
-            # ,text=True
-            ,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE
-        )
-        std = process_output.communicate(b'\n')
-        logging.info('ffmpeg output: \n{0}'.format(std[1].decode('ascii')))
+            std = process_output.communicate(b'\n')
+            logging.info('ffmpeg output: \n{0}'.format(std[1].decode('ascii')))
 
-if is_main:
+if __name__ == '__main__':
     path = r"C:\Users\jarre\Desktop\Videos\Meme & Dede's 60th Anniversary"
     files = get_chaptered(path)
     join_chaptered(files,output)
